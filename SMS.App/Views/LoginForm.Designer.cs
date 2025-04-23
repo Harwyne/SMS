b312c585ee624a9a64588bdf4b2ad5924ab960d4
Jay Al Gallenero <jeegallenero@gmail.com>
2025-04-23 14:28:29 +0800

    Login Function

---
+++ SMS.App/Views/LoginForm.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace SMS.App.Views
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBoxUserName = new TextBox();
+            textBoxPassword = new TextBox();
+            buttonLogin = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(231, 150);
+            label1.Name = "label1";
+            label1.Size = new Size(75, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Username";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(231, 205);
+            label2.Name = "label2";
+            label2.Size = new Size(70, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Password";
+            // 
+            // textBoxUserName
+            // 
+            textBoxUserName.Location = new Point(323, 147);
+            textBoxUserName.Name = "textBoxUserName";
+            textBoxUserName.Size = new Size(247, 27);
+            textBoxUserName.TabIndex = 2;
+            // 
+            // textBoxPassword
+            // 
+            textBoxPassword.Location = new Point(323, 202);
+            textBoxPassword.Name = "textBoxPassword";
+            textBoxPassword.Size = new Size(247, 27);
+            textBoxPassword.TabIndex = 3;
+            // 
+            // buttonLogin
+            // 
+            buttonLogin.Location = new Point(361, 275);
+            buttonLogin.Name = "buttonLogin";
+            buttonLogin.Size = new Size(94, 29);
+            buttonLogin.TabIndex = 4;
+            buttonLogin.Text = "Login";
+            buttonLogin.UseVisualStyleBackColor = true;
+            buttonLogin.Click += buttonLogin_Click;
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(buttonLogin);
+            Controls.Add(textBoxPassword);
+            Controls.Add(textBoxUserName);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "LoginForm";
+            Text = "LoginForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBoxUserName;
+        private TextBox textBoxPassword;
+        private Button buttonLogin;
+    }
+}
---
+++ SMS.Infastructure/Data/AppDbSeed.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SMS.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Infastructure.Data
+{
+    public class AppDbSeed
+    {
+        public static void SeedUsers(ModelBuilder builder)
+        {
+            var passwordHasher = new PasswordHasher<AppUser>();
+
+            var appUser = new AppUser{
+                Id = Guid.NewGuid().ToString(),
+                UserName = "admin", 
+                PasswordHash = passwordHasher.HashPassword(null, "admin"),
+            };
+
+            builder.Entity<AppUser>().HasData(appUser);
+
+        }
+    }
+}
